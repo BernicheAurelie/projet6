086e758d6ca4ae11d6fd9046f7a04084c673ac68
Aurelie Berniche <berniche.aurelie@orange.fr>
2021-08-09 08:26:59 +0200

    update + readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # projet6
+
+[![Generic badge](https://img.shields.io/badge/MADE_WITH-HTML5-orange.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/MADE_WITH-CSS3-blue.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/APPROVED_BY-AURELIE_BERNICHE-blueviolet.svg)](https://shields.io/)
+
+
+## Introduction
+
+Ce site web a été réalisé pour une association qui souhaite se doter d'une application pour visualiser en temps réel un classement de films interessants.
+Ce site fonctionne sur les trois navigateurs principaux et est en lien avec une API maison permettant de recueillir les données filtrées des films grâce à des requètes AJAX.
+
+### Pré-requis
+
+- Editeur de texte [Download VS Code](https://code.visualstudio.com/) 
+- Editeur de texte [Download sublime Text](https://www.sublimetext.com/download)
+
+### Installation
+
+1) Dans un premier temps, vous devez installer la version locale de l'API OCMovies:  
+[Dépot de code de l'API](https://github.com/OpenClassrooms-Student-Center/OCMovies-API-EN-FR.git) 
+2) Suivez ensuite les instructions du Readme pour installer l'environnement virtuel et lancer le serveur:
+[Readme](https://github.com/OpenClassrooms-Student-Center/OCMovies-API-EN-FR#readme)
+3) Consulter le répository contenant les codes du site:
+[Dépot Aurélie Berniche](https://github.com/BernicheAurelie/projet6.git)
+4) Cloner le avec la commande suivante dans le terminal:
+```git clone https://github.com/BernicheAurelie/projet6.git```
+
+## Démarrage
+
+Vous pouvez maintenant lancer le site web:
+1) *Double cliquez* sur le fichier **index.html** ou *clic droit*: ```ouvrir avec``` et choisir un navigateur internet. 
+2) Les meilleurs films vont automatiquement être chargés dans l'interface. 
+3) Cliquez ensuite sur l'image du film qui vous intéresse pour avoir les informations venant de l'API dans un fenêtre modale.
+4) Un bouton permet de fermer la fenêtre ou cliquez sur tout autre endroit de la page.
+
+## Fabriqué avec
+
+- [HTML5](https://html.spec.whatwg.org/) - Format de données
+- [CSS3](https://html.spec.whatwg.org/) - Feuille de style en cascade
+- [Visual Studio Code](https://code.visualstudio.com/) - Editeur de textes
+
+## Contributing
+
+**Ranga Gonnage** _[Mentor OpenClassrooms](https://openclassrooms.com/fr/)_
+
+## Versions
+
+**Dernière version :** 1.0
+
+## Auteurs
+
+**Aurélie Berniche** _[lien GitHub](https://github.com/BernicheAurelie)_
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const animation_movies_slider = document.querySelector(".animation_movies_group"
 const adventure_movies_slider = document.querySelector(".adventure_movies_group")
 
 var scrollPerClick;
-var ImagePadding = 50
+var ImagePadding = 40
 
 let best_movies_url = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
 let family_movies_url = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=Family&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
@@ -12,29 +12,24 @@ let animation_movies_url = "http://localhost:8000/api/v1/titles/?year=&min_year=
 let adventure_movies_url = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=Adventure&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
 
 
-showMovieData(best_movies_url, "best_movies")
-showMovieData(family_movies_url, "family_movies")
-showMovieData(animation_movies_url, "animation_movies")
-showMovieData(adventure_movies_url, "adventure_movies")
-
 // get bestmovie from api => return json =>set attribute id to button with film id
 // and image_url for image.
 const getBestMovie = fetch("http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=9&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=")
-.then(function(res) {
+    .then(function(res) {
   if (res.ok) {
     return res.json();
   }
-})
-.then(function(value){
-    let best_movie = document.querySelector("img");
-    best_movie.setAttribute("src",value.results[0].image_url);
-    best_movie.setAttribute("id", value.results[0].id);
-    let best_movie_title=document.getElementById("meilleurFilm_title");
-    best_movie_title.innerText = value.results[0].title;
-})
-.catch(function(err){
-console.log("une erreur est survenue: ", err);
-});
+    })
+    .then(function(value){
+        let best_movie = document.querySelector("img");
+        best_movie.setAttribute("src",value.results[0].image_url);
+        best_movie.setAttribute("id", value.results[0].id);
+        let best_movie_title=document.getElementById("meilleurFilm_title");
+        best_movie_title.innerText = value.results[0].title;
+    })
+    .catch(function(err){
+        console.log("An error append: ", err);
+    });
 
 var scrollAmount = 0;
 
@@ -46,7 +41,7 @@ function sliderScrollLeft(selected_class){
     });
 
     if(scrollAmount < 0){
-        scrollAmount = 0
+        scrollAmount = 920
     }
 }
 
@@ -67,24 +62,49 @@ function sliderScrollRight(selected_class){
     }
 }
 
+
 async function showMovieData(url, className){
-    var result = await axios.get(url);
-    var result_page2 = []
-    var result2 = result.data.next
-    result2 = await axios.get(result2)
-    var result6 = result2.data.results[0]
-    var result7 = result2.data.results[1]
-    result_page2.push(result6)
-    result_page2.push(result7)
-    result = result.data.results;
-    var movies = result.concat(result_page2)
-    let images = document.getElementsByClassName(className);
-    movies.map(function(cur,index){
-        images[index].setAttribute("src", cur.image_url);
-        images[index].setAttribute("id", cur.id);
-    });
-    scrollPerClick = images[0].clientWidth + ImagePadding;
+    fetch(url)
+        .then(function(res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(value){
+            let all_results = value.results 
+            let results2 = value.next
+            const  getPage2 = fetch(results2)
+                .then(function(res) {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                })
+                .then(function(value){
+                    let result6 = value.results[0]
+                    let result7 = value.results[1]
+                    let result_page2 = []
+                    result_page2.push(result6)
+                    result_page2.push(result7)
+                    let movies = all_results.concat(result_page2)
+                    let images = document.getElementsByClassName(className);
+                    movies.map(function(cur,index){
+                    images[index].setAttribute("src", cur.image_url);
+                    images[index].setAttribute("id", cur.id);
+                    });
+                    scrollPerClick = images[0].clientWidth + ImagePadding;
+                })
+                .catch(function(err){
+                    console.log("An error append fetching second page ", err);
+                    });
+        })
 }
+
+//
+
+showMovieData(best_movies_url, "best_movies")
+showMovieData(family_movies_url, "family_movies")
+showMovieData(animation_movies_url, "animation_movies")
+showMovieData(adventure_movies_url, "adventure_movies")
 
 // get movie informations for modal box
 function getMovieInformation(movie_url){
@@ -131,16 +151,19 @@ function getMovieInformation(movie_url){
             .innerText =`Directors: ${value.directors}`;
         document
             .getElementById("actors")
-            .innerText = `Actors:\n${value.actors}`;
+            .innerText = value.actors;
         document
             .getElementById("description")
-            .innerText = `Description:\n${value.description}`;
+            .innerText = value.description;
         document
             .getElementById("image_url_cible")
             .setAttribute("src",value.image_url);
+        // document
+        //     .getElementById("imdb_url")
+        //     .setAttribute("href", value.imdb_url);
     })
     .catch(function(err) {
-        console.log("une erreur est survenue.")
+        console.log("An error append", err)
         });
 }
 
@@ -154,10 +177,14 @@ for (let i = 0; i < imgs.length; i++) {
     imgs[i].addEventListener("click", function(e) {
         e.preventDefault();
         modal.style.display = "block";
-        //let id_movie = e.target.getAttribute("id");
         let id_movie=imgs[i].getAttribute("id");
         let movie_url = `http://localhost:8000/api/v1/titles/${id_movie}`.split("%20");
-        getMovieInformation(movie_url)
+        if (id_movie === "image_url_cible") {
+            console.log("datas are already loaded");
+        } else {
+            getMovieInformation(movie_url);
+        }
+        
   })  
 }
 // Get the <span> element that closes the modal and
